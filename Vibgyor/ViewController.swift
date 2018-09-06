82f8df7bd7504be1b1aff6547ccd9eb61c64aa7f
Harshavardhan K <harshavardhan816@gmail.com>
2018-09-06 23:39:42 +0530

    Initial Commit

---
+++ Vibgyor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vibgyor
+//
+//  Created by Harshavardhan K on 06/09/18.
+//  Copyright © 2018 Harshavardhan K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
